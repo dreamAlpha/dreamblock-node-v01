ae086a985ae3fba7b94081e754d426c1c7a08f67
LM <dev@dreamblock.com>
2019-02-02 10:57:06 +0200

    Multichain version and server change

--- node-linux/cookbooks/multi_chain/attributes/default.rb
+++ node-linux/cookbooks/multi_chain/attributes/default.rb
@@ -1,9 +1,7 @@
 default["app"]["directory"] = "/apps"
 default["data"]["directory"] = "/datadir"
 default["temp"]["directory"] = "/tmp"
-default["software"]["multichain"] = "multichain-1.0.6.tar.gz"
-default["blockchain"]["name"] = "pxtmain"
-default["blockchain"]["ip"] = "154.0.160.51"
-default["blockchain"]["port"] = "4783"
-
-
+default["software"]["multichain"] = "multichain-2.0-beta-1.tar.gz"
+default["blockchain"]["name"] = "testchain"
+default["blockchain"]["ip"] = "139.180.202.167"
+default["blockchain"]["port"] = "2651"
