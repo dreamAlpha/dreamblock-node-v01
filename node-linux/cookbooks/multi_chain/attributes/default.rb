2175be604bd7ee8d927f9f745a46a16cf56f95ca
Avi <avi.tenzer@rmb.co.za>
2019-03-07 08:15:28 +0200

    updated ip address

--- node-linux/cookbooks/multi_chain/attributes/default.rb
+++ node-linux/cookbooks/multi_chain/attributes/default.rb
@@ -4,6 +4,6 @@ default["temp"]["directory"] = "/tmp"
 
 default["software"]["multichain"] = "multichain-2.0-beta-1.tar.gz"
 default["blockchain"]["name"] = "testchain"
-default["blockchain"]["ip"] = "139.180.202.167"
+default["blockchain"]["ip"] = "167.179.78.6"
 default["blockchain"]["port"] = "2651"
 
