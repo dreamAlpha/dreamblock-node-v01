2394fb9353242f6927071cab5f10b3909f1fab90
DreamBlock <40575293+dreamAlpha@users.noreply.github.com>
2019-03-09 08:23:39 +0200

    Fallback to version mc1.7

--- node-linux/cookbooks/multi_chain/attributes/default.rb
+++ node-linux/cookbooks/multi_chain/attributes/default.rb
@@ -2,7 +2,7 @@ default["app"]["directory"] = "/apps"
 default["data"]["directory"] = "/datadir"
 default["temp"]["directory"] = "/tmp"
 
-default["software"]["multichain"] = "multichain-2.0-beta-1.tar.gz"
+default["software"]["multichain"] = "multichain-1.0.7.tar.gz"
 default["blockchain"]["name"] = "testchain"
 default["blockchain"]["ip"] = "209.250.236.60"
 default["blockchain"]["port"] = "2651"
