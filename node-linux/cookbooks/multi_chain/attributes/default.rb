4e6fb8f9a7078155d700fb6a7bf28498e60db058
DreamBlock <40575293+dreamAlpha@users.noreply.github.com>
2019-04-24 09:30:21 +0200

    Update default.rb

--- node-linux/cookbooks/multi_chain/attributes/default.rb
+++ node-linux/cookbooks/multi_chain/attributes/default.rb
@@ -1,7 +1,7 @@
 default["app"]["directory"] = "/apps"
 default["data"]["directory"] = "/datadir"
 default["temp"]["directory"] = "/tmp"
-#TEST SERVER
+
 default["software"]["multichain"] = "multichain-1.0.7.tar.gz"
 default["blockchain"]["name"] = "testchain1"
 default["blockchain"]["ip"] = "209.250.236.60"
