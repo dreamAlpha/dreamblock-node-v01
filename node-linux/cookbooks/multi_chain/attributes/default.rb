3d9e4b8c7e7f8a7c2a0b8b9cd950660488b9c60d
DreamBlock <40575293+dreamAlpha@users.noreply.github.com>
2019-05-30 10:38:24 +0200

    Update default.rb

--- node-linux/cookbooks/multi_chain/attributes/default.rb
+++ node-linux/cookbooks/multi_chain/attributes/default.rb
@@ -3,6 +3,6 @@ default["data"]["directory"] = "/datadir"
 default["temp"]["directory"] = "/tmp"
 
 default["software"]["multichain"] = "multichain-1.0.7.tar.gz"
-default["blockchain"]["name"] = "testchain1"
+default["blockchain"]["name"] = "frxtest"
 default["blockchain"]["ip"] = "209.250.236.60"
 default["blockchain"]["port"] = "4329"
